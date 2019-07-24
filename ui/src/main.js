f5726a6be19efdd49a98b4aa2655389736fbfcbd
Administator <knucker@KnuckerPc.localdomain>
2019-07-24 20:44:12 +0300

    refactor to vuex alpha stage

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,12 +3,15 @@ import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { createStore } from './store'
 
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+const store = createStore();
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
--- ui/src/store.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-import axios from 'axios'
-import Vuex from 'vuex'
